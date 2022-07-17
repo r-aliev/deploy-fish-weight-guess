ec11b15ed1a760b72866eff7b45706a87ce0f3c5
Rashid <rashid.aliev@sp3d.co>
2022-07-18 00:20:16 +0200

    again

--- api/apps.py
+++ api/apps.py
@@ -2,12 +2,12 @@ from django.apps import AppConfig
 import os
 import pickle
 
-class ApiConfig(AppConfig):
 
+class ApiConfig(AppConfig):
+    from .utils import PredictionModel
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
     CURRENT_DIR = os.path.dirname(__file__)
     model_file = os.path.join(CURRENT_DIR, 'weight-prediction.model')
 
     model = pickle.load(open(model_file, 'rb'))
-
--- manage.py
+++ manage.py
@@ -19,5 +19,5 @@ def main():
 
 
 if __name__ == '__main__':
-    from core.settings import PredictionModel
+    from api.utils import PredictionModel
     main()
