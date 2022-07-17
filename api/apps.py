52f9385771bdd22c4a0e1399f740fd15fa1cab0f
Rashid <rashid.aliev@sp3d.co>
2022-07-17 23:09:31 +0200

    try to fix issue related to model

--- api/apps.py
+++ api/apps.py
@@ -2,15 +2,5 @@ from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
-    import pickle
-
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-
-    path = 'api/weight-prediction.model'
-
-    model = None
-    try:
-        model = pickle.load(open(path, 'rb'))
-    except EOFError as e:
-        print(e)
--- api/views.py
+++ api/views.py
@@ -6,7 +6,14 @@ from rest_framework.decorators import parser_classes
 from rest_framework.parsers import MultiPartParser
 from .apps import ApiConfig
 from .utils import format_float
+import os
+import pickle
+from .utils import PredictionModel
 
+CURRENT_DIR = os.path.dirname(__file__)
+model_file = os.path.join(CURRENT_DIR, 'weight-prediction.model')
+
+model = pickle.load(open(model_file, 'rb'))
 
 @api_view(['GET', 'POST'])
 @parser_classes([MultiPartParser])
@@ -16,7 +23,7 @@ def guess_fishes(request):
         file_path = 'api/fish_samples.csv'
         data = pd.read_csv(file_path)
         data.rename(columns={data.columns[0]: "Weight"}, inplace=True)
-        print(data)
+
         return JsonResponse(data={'sampleData': data.to_json(orient='records')}, status=status.HTTP_200_OK)
 
     if request.method == 'POST':
@@ -25,14 +32,13 @@ def guess_fishes(request):
         if file_obj:
             data = pd.read_csv(file_obj)
 
-            model = ApiConfig.model
+            if model:
+                prediction = model.predict(data)
+                format_float(prediction)
 
-            prediction = model.predict(data)
-            format_float(prediction)
+                data['Weight'] = prediction
 
-            data['Weight'] = prediction
-
-            return JsonResponse(data={'prediction': data.to_json(orient='records')}, status=status.HTTP_200_OK)
+                return JsonResponse(data={'prediction': data.to_json(orient='records')}, status=status.HTTP_200_OK)
 
         return JsonResponse(data={"message": f"File was not uploaded, try again please"},
                             status=status.HTTP_404_NOT_FOUND)
